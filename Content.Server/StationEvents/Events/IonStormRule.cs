debaa8178942c2cf61c3fe7cca970b5f1d1dee50
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-05-27 22:27:42 -0700

    expand namespace in comments

--- Content.Server/StationEvents/Events/IonStormRule.cs
+++ Content.Server/StationEvents/Events/IonStormRule.cs
@@ -4,19 +4,19 @@ using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
 using Content.Shared.Silicons.Laws.Components;
 using Content.Shared.Station.Components;
-// CD - start synth trait
+// Cosmatic Drift - start synth trait
 using Content.Server.Chat.Managers;
 using Content.Shared.Chat;
 using Robust.Shared.Player;
 using Robust.Shared.Random;
-// CD - end synth trait
+// Cosmatic Drift - end synth trait
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
 {
     [Dependency] private readonly IonStormSystem _ionStorm = default!;
-    [Dependency] private readonly IChatManager _chatManager = default!; // CD - Used for synth trait
+    [Dependency] private readonly IChatManager _chatManager = default!; // Cosmatic Drift - Used for synth trait
 
     protected override void Started(EntityUid uid, IonStormRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
@@ -25,7 +25,7 @@ public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
         if (!TryGetRandomStation(out var chosenStation))
             return;
 
-        // CD - Go through everyone with the SynthComponent and inform them a storm is happening.
+        // Cosmatic Drift - Go through everyone with the SynthComponent and inform them a storm is happening.
         var synthQuery = EntityQueryEnumerator<SynthComponent>();
         while (synthQuery.MoveNext(out var ent, out var synthComp))
         {
@@ -39,7 +39,7 @@ public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
             var wrappedMessage = Loc.GetString("chat-manager-server-wrap-message", ("message", msg));
             _chatManager.ChatMessageToOne(ChatChannel.Server, msg, wrappedMessage, default, false, actor.PlayerSession.Channel, colorOverride: Color.Yellow);
         }
-        // CD - End of synth trait
+        // Cosmatic Drift - End of synth trait
 
         var query = EntityQueryEnumerator<SiliconLawBoundComponent, TransformComponent, IonStormTargetComponent>();
         while (query.MoveNext(out var ent, out var lawBound, out var xform, out var target))
